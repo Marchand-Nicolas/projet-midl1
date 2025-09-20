cc143840422bafac348e90924918e7a776c2b004
robinou <robin.dm@outlook.fr>
2025-09-20 13:25:58 +0200

    Create syntax.ml

---
+++ syntax.ml
@@ -0,0 +1,88 @@
+(* Symbols : ⊤ ⊥ ∧ ∨ ¬ = < ⩽ > ⩾ ∀ ∃ *)
+
+open Printf
+
+type compar_op =
+  | Equal
+  | Lt
+  | Gt
+;;
+
+type bool_const =
+  | Top     (*"true"*)
+  | Bottom (*"false"*)
+;; 
+
+type bool_op =
+  | Conj 
+  | Disj
+;;
+
+type quantif =
+  | Exists
+  | Forall
+
+type formula =
+  | Const of bool_const
+  | Variable of string
+  | Number of float
+  | ComparF of formula * compar_op * formula
+  | BoolF of formula * bool_op * formula
+  | NotF of formula
+  | Quantif of quantif * formula * formula
+;;
+
+let rep_quantif = function
+  | Exists -> "∃"
+  | Forall -> "∀"
+;;
+
+let rep_const = function
+  | Top -> "Τ"
+  | Bottom -> "⊥"
+;;
+
+let rep_comp_op = function
+  | Equal -> "="
+  | Lt -> "<"
+  | Gt -> ">"
+
+let rep_bool_op = function
+  | Conj -> "∧"
+  | Disj -> "∨"
+;;
+
+let rep_not = "¬";;
+
+let rep_number = string_of_float;;
+
+let rec rep_formula = function
+  | Const c -> rep_const c
+  | ComparF(f, op, g) -> sprintf
+  "(%s %s %s)" (rep_formula f) (rep_comp_op op) (rep_formula g)
+  | BoolF(f, op, g) -> sprintf
+  "(%s %s %s)" (rep_formula f) (rep_bool_op op) (rep_formula g)
+  | NotF f -> sprintf
+  "%s%s" rep_not (rep_formula f)
+  | Quantif(q, Variable v, f) -> sprintf
+  "(%s %s %s)" (rep_quantif q) v (rep_formula f)
+  | Variable v -> v
+  | Number n -> rep_number n
+  | _ -> failwith("Erreur dans la représentation de la formule")
+;;
+
+let top = Const(Top);;
+let bottom = Const(Bottom);;
+let forall v f = Quantif(Forall, v, f);;
+let exists v f = Quantif(Exists, v, f);;
+let equal f g = ComparF(f,Equal,g);;
+let gt f g = ComparF(f, Gt, g);;
+let lt f g = ComparF(f,Lt, g);;
+let notf f = NotF(f);;
+let conj f g = BoolF(f,Conj,g);;
+let disj f g = BoolF(f,Disj,g);;
+let var x = Variable(x);;
+
+let rec print_formula f = print_string (rep_formula f ^ "\n");;
+
+print_formula (forall (var "x") (exists (var "y") (gt (var "x" ) (var "y"))));;
